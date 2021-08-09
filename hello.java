0e8a76bbd9ab696f449b99ee37b7ecd071ac2105
ChristofB7 <Christof@jazab.com>
2021-08-09 15:36:07 -0400

    added hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, Christof!");
+	System.out.println("You live at: 123 Madeup St");
+	System.out.println("Your number is: 555-555-9816");
+	System.out.println("Your current city is: Main City, population: args");
+    }
+}
